29c16dbff85be0a31f803bf9d1a799e0ea666a04
Kevin Tamaki <tamakikevin@gmail.com>
2022-04-22 15:18:32 -0700

    added styles and finishing touches to functionality & performance

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
+
+import './card-list.styles.css';
 
 class CardList extends Component {
 
@@ -7,12 +10,15 @@ class CardList extends Component {
         const { monsters } = this.props;
 
         return (
-            <div>
-                {monsters.map(monster =>(
-                    <h1 key={monster.id}> {monster.name} </h1>
-                ))}
+            <div className='card-list'>
+                {monsters.map((monster) => {
+
+                    return (
+                        <Card monster={monster} />
+                    );
+                })}
             </div>
-        )
+        );
     }
 }
 
